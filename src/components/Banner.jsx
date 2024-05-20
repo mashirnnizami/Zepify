4fb627fdf152895361ecf656fa319ef370855641
mashirdav123 <mashirdav123@gmail.com>
2024-05-20 11:49:20 +0000

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -19,7 +19,7 @@ function Banner() {
 </label>
 
     </div>
-    <button className="btn mt-6 btn-primary">Secondary</button>
+    <button className="btn mt-6 btn-primary">Subscribe here</button>
 
     </div>
     <div className='order-1 w-full md:w-1/2'>
